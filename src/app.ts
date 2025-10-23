b7650cc4ecb0e4e9f5cb55559fa7fff84f3999fc
GianfrancoGrigera <gianfrancogrigera@gmail.com>
2025-10-23 19:31:07 -0300

    Conexión a DB

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Application } from "express";
 import cors from "cors";
+import connectDB from "./config/db";
+import dotenv from "dotenv";
 
+
+dotenv.config();
 const app: Application = express();
 
 // Middlewares
@@ -8,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Conexión a la base de datos
-// connectDB();
+connectDB();
 
 // Rutas
 // app.use("/api/auth", authRoutes);
